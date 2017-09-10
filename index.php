5c46fc9084ff14be5e5231c35b20c725fe226cca
Jacob <421107871@qq.com>
2017-09-10 21:00:41 +0800

    首页入口路径调整

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 define('APP_NAME','ROOT');
 define('APP_PATH','./Root/');
-define('APP_DEBUG',true);
-require 'E:/FrameWork/ThinkPHP3.1.3/ThinkPHP.php';
-//require './ThinkPHP/ThinkPHP.php';
+define('APP_DEBUG',false);
+require './ThinkPHP/ThinkPHP.php';
 ?>
